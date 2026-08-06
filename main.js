0e27fa9def23090c565bf453692bdcf157cf0e48
rahmatullahdms <rahmatullah.dms@gmail.com>
2026-08-06 06:37:55 +0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".auto-type", {
+    strings: [
+        "SEO Expert",
+        "WordPress Developer",
+        "Digital Marketing Mentor",
+        "Google Ads Expert",
+        "Facebook Ads Expert"
+    ],
+    typeSpeed: 80,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true
+});
